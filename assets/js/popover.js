6846d314d654426e058269464978b365adc239b5
Claudio Yanes <me@claudio4.com>
2022-03-04 02:07:51 +0000

    Move css and js to appropriate files
    Having the CSS and JS in the html template produces pages larger
    than necessary, as each page need to contain all the js/css.
    Separating them in appropriate files allow the browser to just download
    them once and use them for all the pages. This is even more effective
    with an aggressive cache policy for the js and css, something that can
    be done without fear thanks to the implemented cache-busting.
    Also, having then in separate files allows us to use Hugo pipelines
    for minimizing the code.

---
+++ assets/js/popover.js
@@ -0,0 +1,34 @@
+function htmlToElement(html) {
+    const template = document.createElement('template')
+    html = html.trim()
+    template.innerHTML = html
+    return template.content.firstChild
+}
+
+function initPopover(base) {
+    const baseUrl = base.replace(window.location.origin, "") // is this useless?
+    document.addEventListener("DOMContentLoaded", () => {
+        fetchData().then(({content}) => {
+        const links = [...document.getElementsByClassName("internal-link")]
+        links.forEach(li => {
+            const linkDest = content[li.dataset.src.replace(baseUrl, "")]
+            // const linkDest = content[li.dataset.src]
+            if (linkDest) {
+                const popoverElement = `<div class="popover">
+    <h3>${linkDest.title}</h3>
+    <p>${removeMarkdown(linkDest.content).split(" ", 20).join(" ")}...</p>
+    <p class="meta">${new Date(linkDest.lastmodified).toLocaleDateString()}</p>
+</div>`
+                const el = htmlToElement(popoverElement)
+                li.appendChild(el)
+                li.addEventListener("mouseover", () => {
+                el.classList.add("visible")
+                })
+                li.addEventListener("mouseout", () => {
+                el.classList.remove("visible")
+                })
+            }
+            })
+        })
+    })
+}
