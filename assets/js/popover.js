c92332681a7671124257a9e838fb93cf18b80887
Jacky Zhao <j.zhao2k19@gmail.com>
2022-04-05 20:44:39 -0700

    fix: check for src before attempting to add popover

--- assets/js/popover.js
+++ assets/js/popover.js
@@ -10,24 +10,26 @@ function initPopover(baseURL) {
   document.addEventListener("DOMContentLoaded", () => {
     fetchData.then(({ content }) => {
       const links = [...document.getElementsByClassName("internal-link")]
-      links.forEach(li => {
-        const linkDest = content[li.dataset.src.replace(basePath, "")]
-        if (linkDest) {
-          const popoverElement = `<div class="popover">
+      links
+        .filter(li => li.dataset.src)
+        .forEach(li => {
+          const linkDest = content[li.dataset.src.replace(basePath, "")]
+          if (linkDest) {
+            const popoverElement = `<div class="popover">
     <h3>${linkDest.title}</h3>
     <p>${removeMarkdown(linkDest.content).split(" ", 20).join(" ")}...</p>
     <p class="meta">${new Date(linkDest.lastmodified).toLocaleDateString()}</p>
 </div>`
-          const el = htmlToElement(popoverElement)
-          li.appendChild(el)
-          li.addEventListener("mouseover", () => {
-            el.classList.add("visible")
-          })
-          li.addEventListener("mouseout", () => {
-            el.classList.remove("visible")
-          })
-        }
-      })
+            const el = htmlToElement(popoverElement)
+            li.appendChild(el)
+            li.addEventListener("mouseover", () => {
+              el.classList.add("visible")
+            })
+            li.addEventListener("mouseout", () => {
+              el.classList.remove("visible")
+            })
+          }
+        })
     })
   })
 }
