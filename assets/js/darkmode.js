2816e51428fc8ee04218c0d319d8dd5de7a3b583
Matthew Wong <matthewwong525@gmail.com>
2022-05-04 10:53:45 -0400

    update header to be responsive

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -6,24 +6,32 @@ if (currentTheme) {
 }
 
 const switchTheme = (e) => {
-  if (e.target.checked) {
+  if (e.checked) {
     document.documentElement.setAttribute('saved-theme', 'dark')
     localStorage.setItem('theme', 'dark')
+    document.querySelector('#darkmode-icon').setAttribute('class', 'fa fa-moon-o')
   }
   else {
     document.documentElement.setAttribute('saved-theme', 'light')
     localStorage.setItem('theme', 'light')
+    document.querySelector('#darkmode-icon').setAttribute('class', 'fa fa-sun-o')
   }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
   // Darkmode toggle
   const toggleSwitch = document.querySelector('#darkmode-toggle')
+  const dmSwitch = document.querySelector('#darkmode-switch')
 
   // listen for toggle
-  toggleSwitch.addEventListener('change', switchTheme, false)
+  dmSwitch.addEventListener('click', (e) => {
+    toggleSwitch.checked = !toggleSwitch.checked;
+    switchTheme(toggleSwitch);
+  }, false);
+
 
   if (currentTheme === 'dark') {
     toggleSwitch.checked = true
+    document.querySelector('#darkmode-icon').setAttribute('class', 'fa fa-moon-o')
   }
 })
