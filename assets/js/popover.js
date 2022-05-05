cea0f3eb743b26db0d5297ab10e229617585fe0c
Jacky Zhao <j.zhao2k19@gmail.com>
2022-05-05 00:58:50 -0400

    feat: contextual backlinks (closes #106)

--- assets/js/popover.js
+++ assets/js/popover.js
@@ -5,19 +5,20 @@ function htmlToElement(html) {
   return template.content.firstChild
 }
 
-function initPopover(baseURL) {
+function initPopover(baseURL, useContextualBacklinks) {
   const basePath = baseURL.replace(window.location.origin, "")
   document.addEventListener("DOMContentLoaded", () => {
     fetchData.then(({ content }) => {
       const links = [...document.getElementsByClassName("internal-link")]
       links
-        .filter(li => li.dataset.src)
+        .filter(li => li.dataset.src || (li.dataset.idx && useContextualBacklinks))
         .forEach(li => {
-          const linkDest = content[li.dataset.src.replace(/\/$/g, "").replace(basePath, "")]
-          if (linkDest) {
+          if (li.dataset.ctx) {
+            console.log(li.dataset.ctx)
+            const linkDest = content[li.dataset.src]
             const popoverElement = `<div class="popover">
     <h3>${linkDest.title}</h3>
-    <p>${removeMarkdown(linkDest.content).split(" ", 20).join(" ")}...</p>
+    <p>${highlight(removeMarkdown(linkDest.content), li.dataset.ctx)}...</p>
     <p class="meta">${new Date(linkDest.lastmodified).toLocaleDateString()}</p>
 </div>`
             const el = htmlToElement(popoverElement)
@@ -28,6 +29,23 @@ function initPopover(baseURL) {
             li.addEventListener("mouseout", () => {
               el.classList.remove("visible")
             })
+          } else {
+            const linkDest = content[li.dataset.src.replace(/\/$/g, "").replace(basePath, "")]
+            if (linkDest) {
+              const popoverElement = `<div class="popover">
+    <h3>${linkDest.title}</h3>
+    <p>${removeMarkdown(linkDest.content).split(" ", 20).join(" ")}...</p>
+    <p class="meta">${new Date(linkDest.lastmodified).toLocaleDateString()}</p>
+</div>`
+              const el = htmlToElement(popoverElement)
+              li.appendChild(el)
+              li.addEventListener("mouseover", () => {
+                el.classList.add("visible")
+              })
+              li.addEventListener("mouseout", () => {
+                el.classList.remove("visible")
+              })
+            }
           }
         })
     })
