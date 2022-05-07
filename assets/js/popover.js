bf3e060a13f86b1ca1c71f3f1bdab6c54261bcd8
Jet Hughes <hugje043@student.otago.ac.nz>
2022-05-07 21:44:30 +1200

    updated base quartz

--- assets/js/popover.js
+++ assets/js/popover.js
@@ -1,35 +1,58 @@
 function htmlToElement(html) {
-  const template = document.createElement('template')
+  const template = document.createElement("template")
   html = html.trim()
   template.innerHTML = html
   return template.content.firstChild
 }
 
-function initPopover(baseURL) {
+function initPopover(baseURL, useContextualBacklinks, renderLatex) {
   const basePath = baseURL.replace(window.location.origin, "")
-  document.addEventListener("DOMContentLoaded", () => {
-    fetchData.then(({ content }) => {
-      const links = [...document.getElementsByClassName("internal-link")]
-      links
-        .filter(li => li.dataset.src)
-        .forEach(li => {
-          const linkDest = content[li.dataset.src.replace(basePath, "")]
+  fetchData.then(({ content }) => {
+    const links = [...document.getElementsByClassName("internal-link")]
+    links
+      .filter(li => li.dataset.src || (li.dataset.idx && useContextualBacklinks))
+      .forEach(li => {
+        var el
+        if (li.dataset.ctx) {
+          const linkDest = content[li.dataset.src]
+          const popoverElement = `<div class="popover">
+    <h3>${linkDest.title}</h3>
+    <p>${highlight(removeMarkdown(linkDest.content), li.dataset.ctx)}...</p>
+    <p class="meta">${new Date(linkDest.lastmodified).toLocaleDateString()}</p>
+</div>`
+          el = htmlToElement(popoverElement)
+        } else {
+          const linkDest = content[li.dataset.src.replace(/\/$/g, "").replace(basePath, "")]
           if (linkDest) {
             const popoverElement = `<div class="popover">
     <h3>${linkDest.title}</h3>
     <p>${removeMarkdown(linkDest.content).split(" ", 20).join(" ")}...</p>
     <p class="meta">${new Date(linkDest.lastmodified).toLocaleDateString()}</p>
 </div>`
-            const el = htmlToElement(popoverElement)
-            li.appendChild(el)
-            li.addEventListener("mouseover", () => {
-              el.classList.add("visible")
-            })
-            li.addEventListener("mouseout", () => {
-              el.classList.remove("visible")
+            el = htmlToElement(popoverElement)
+          }
+        }
+
+        if (el) {
+          li.appendChild(el)
+          if (renderLatex) {
+            renderMathInElement(el, {
+              delimiters: [
+                { left: '$$', right: '$$', display: false },
+                { left: '$', right: '$', display: false },
+                { left: '\\(', right: '\\)', display: false },
+                { left: '\\[', right: '\\]', display: false }
+              ],
+              throwOnError: false
             })
           }
-        })
-    })
+          li.addEventListener("mouseover", () => {
+            el.classList.add("visible")
+          })
+          li.addEventListener("mouseout", () => {
+            el.classList.remove("visible")
+          })
+        }
+      })
   })
 }
