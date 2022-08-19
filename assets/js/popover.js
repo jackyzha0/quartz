ea89ce7998f2222a84c315b43295090d11cf510e
Vincent Huang <vincenthuang75025@yahoo.com>
2022-08-19 00:40:07 -0500

    Popover preview should show relevant heading

--- assets/js/popover.js
+++ assets/js/popover.js
@@ -24,9 +24,16 @@ function initPopover(baseURL, useContextualBacklinks, renderLatex) {
         } else {
           const linkDest = content[li.dataset.src.replace(/\/$/g, "").replace(basePath, "")]
           if (linkDest) {
+            let splitLink = li.href.split("#")
+            let cleanedContent = removeMarkdown(linkDest.content)
+            if (splitLink.length > 1) {
+              let headingName = splitLink[1].replace(/\-/g, " ")
+              let headingIndex = cleanedContent.toLowerCase().indexOf("<b>" + headingName + "</b>")
+              cleanedContent = cleanedContent.substring(headingIndex, cleanedContent.length)
+            }
             const popoverElement = `<div class="popover">
     <h3>${linkDest.title}</h3>
-    <p>${removeMarkdown(linkDest.content).split(" ", 20).join(" ")}...</p>
+    <p>${cleanedContent.split(" ", 20).join(" ")}...</p>
     <p class="meta">${new Date(linkDest.lastmodified).toLocaleDateString()}</p>
 </div>`
             el = htmlToElement(popoverElement)
