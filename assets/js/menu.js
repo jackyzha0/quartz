0c2e278738f1f7a8f81099d1cd2b241043226693
Pawel-Kica <pawel.kica.cc@proton.me>
2022-09-27 11:07:23 +0200

    fixes

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,10 +1,18 @@
 ;(() => {
-  const arrowWrappers = document.getElementsByClassName("a-wrapper")
+  // main
+  const body = document.getElementsByTagName("body")[0]
+  const singlePage = document.getElementsByClassName("singlePage")[0]
+  const blurElement = document.getElementsByClassName("blur-element")[0]
+  // nav
   const titles = document.getElementsByClassName("title")
+  const aside = document.getElementsByClassName("main-aside")[0]
+  const nav_button = document.getElementsByClassName("nav-btn")[0]
+  const arrows_wrappers = document.getElementsByClassName("a-wrapper")
+  const folders = document.getElementsByClassName("folder")
+  const nav_mobile_close_button = document.getElementsByClassName("close-nav-mobile")[0]
 
   let index = 0
-  for (const el of arrowWrappers) {
-    const folders = document.getElementsByClassName("folder")
+  for (const el of arrows_wrappers) {
     const folder = folders[index]
 
     el.addEventListener("click", () => {
@@ -15,60 +23,52 @@
     index++
   }
 
-  const singlePage = document.getElementsByClassName("singlePage")[0]
-  const body = document.getElementsByTagName("body")[0]
-  const blurElement = document.getElementsByClassName("blur-element")[0]
-  const aside = document.getElementsByClassName("main-aside")[0]
-  const nav_button = document.getElementsByClassName("nav-btn")[0]
-
   index = 0
   for (const el of titles) {
-    const folders = document.getElementsByClassName("folder")
     const folder = folders[index]
-
     const arrow = document.getElementsByClassName("arrow")[index]
+
     el.addEventListener("click", () => {
       arrow.classList.add("down")
       folder.classList.add("active")
       body.classList.remove("fixed-position")
       blurElement.classList.remove("enabled")
       aside.classList.add("disabled")
-      // nav_button.classList.add("disabled")
       singlePage.classList.remove("blur")
     })
     index++
   }
 
-  blurElement.addEventListener("click", () => {
-    blurElement.classList.remove("enabled")
-    aside.classList.add("disabled")
-    // nav_button.classList.add("disabled")
-    body.classList.remove("fixed-position")
-    singlePage.classList.remove("blur")
-  })
-
   const pagesLink = document.getElementsByClassName("page-link")
   for (const el of pagesLink) {
     el.addEventListener("click", () => {
       aside.classList.add("disabled")
-      // nav_button.classList.add("disabled")
       body.classList.remove("fixed-position")
       blurElement.classList.remove("enabled")
       singlePage.classList.remove("blur")
     })
   }
 
-  nav_button.addEventListener("click", () => {
-    singlePage.classList.toggle("blur")
-    body.classList.toggle("fixed-position")
-    aside.classList.toggle("disabled")
-    blurElement.classList.toggle("enabled")
-    // nav_button.classList.toggle("disabled")
+  blurElement.addEventListener("click", () => {
+    aside.classList.add("disabled")
+    singlePage.classList.remove("blur")
+    body.classList.remove("fixed-position")
+    blurElement.classList.remove("enabled")
   })
 
-  document.getElementsByClassName("close-nav-mobile")[0].addEventListener("click", () => {
+  nav_button.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" })
+    singlePage.classList.add("blur")
+    aside.classList.remove("disabled")
+    blurElement.classList.add("enabled")
+    // TO-IMPLEMENT - scroll detection to 0
+    setTimeout(() => {
+      body.classList.add("fixed-position")
+    }, 500)
+  })
+
+  nav_mobile_close_button.addEventListener("click", () => {
     aside.classList.add("disabled")
-    // nav_button.classList.add("disabled")
     body.classList.remove("fixed-position")
     blurElement.classList.remove("enabled")
     singlePage.classList.remove("blur")
