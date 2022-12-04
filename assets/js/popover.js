ce5df837f5f6ed57f9e9f85439ee9a40ebf234dc
Jacky Zhao <j.zhao2k19@gmail.com>
2022-12-03 21:03:12 -0800

    feat: latex in search results

--- assets/js/popover.js
+++ assets/js/popover.js
@@ -5,7 +5,7 @@ function htmlToElement(html) {
   return template.content.firstChild
 }
 
-function initPopover(baseURL, useContextualBacklinks, renderLatex) {
+function initPopover(baseURL, useContextualBacklinks) {
   const basePath = baseURL.replace(window.location.origin, "")
   fetchData.then(({ content }) => {
     const links = [...document.getElementsByClassName("internal-link")]
@@ -42,7 +42,7 @@ function initPopover(baseURL, useContextualBacklinks, renderLatex) {
 
         if (el) {
           li.appendChild(el)
-          if (renderLatex) {
+          if (LATEX_ENABLED) {
             renderMathInElement(el, {
               delimiters: [
                 { left: '$$', right: '$$', display: false },
