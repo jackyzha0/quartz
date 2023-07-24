4811500b1b9c169aac82254d771677cd6dc2a86c
Jacky Zhao <j.zhao2k19@gmail.com>
2023-07-23 18:20:43 -0700

    make component resources a proper emitter

--- quartz.config.ts
+++ quartz.config.ts
@@ -95,6 +95,7 @@ const config: QuartzConfig = {
     filters: [Plugin.RemoveDrafts()],
     emitters: [
       Plugin.AliasRedirects(),
+      Plugin.ComponentResources(),
       Plugin.ContentPage({
         ...sharedPageComponents,
         ...contentPageLayout,
