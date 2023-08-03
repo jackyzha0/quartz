558a50916446746768fa43bbdfc8e229d8ec7759
Jacky Zhao <j.zhao2k19@gmail.com>
2023-08-02 22:11:46 -0700

    format

--- quartz/glob.ts
+++ quartz/glob.ts
@@ -1,12 +1,16 @@
-import path from "path";
-import { FilePath } from "./path";
-import { globby } from "globby";
+import path from "path"
+import { FilePath } from "./path"
+import { globby } from "globby"
 
 export function toPosixPath(fp: string): string {
   return fp.split(path.sep).join("/")
 }
 
-export async function glob(pattern: string, cwd: string, ignorePatterns: string[]): Promise<FilePath[]> {
+export async function glob(
+  pattern: string,
+  cwd: string,
+  ignorePatterns: string[],
+): Promise<FilePath[]> {
   const fps = (
     await globby(pattern, {
       cwd,
