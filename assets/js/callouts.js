03f6ab028e91e1c0451146011b39ab394acb5a05
Ryan <139601671+Ryan5358@users.noreply.github.com>
2023-08-05 14:34:22 +1000

    Update callouts.js

--- assets/js/callouts.js
+++ assets/js/callouts.js
@@ -1,6 +1,7 @@
 const addCollapsibleCallouts = () => {
     const collapsibleCallouts = document.querySelectorAll("blockquote.callout-collapsible");
     collapsibleCallouts.forEach(el => el.addEventListener('click', event => {
+        console.log(event);
         el.classList.toggle("callout-collapsed");
     }));
 }
