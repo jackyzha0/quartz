db6054a8c19703345cfdd6dd4a08f35986c14acc
Jacky Zhao <j.zhao2k19@gmail.com>
2023-08-05 18:00:52 -0700

    format, remove markdown from being procesed

--- quartz/build.ts
+++ quartz/build.ts
@@ -4,14 +4,14 @@ sourceMapSupport.install({
     // source map hack to get around query param
     // import cache busting
     if (source.includes(".quartz-cache")) {
-      let realSource = fileURLToPath(source.split("?", 2)[0] + '.map')
+      let realSource = fileURLToPath(source.split("?", 2)[0] + ".map")
       return {
-        map: fs.readFileSync(realSource, 'utf8')
+        map: fs.readFileSync(realSource, "utf8"),
       }
     } else {
       return null
     }
-  }
+  },
 })
 
 import path from "path"
