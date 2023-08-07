b4d211fbd5b71070834a926299d751c2719262c8
Ryan <139601671+Ryan5358@users.noreply.github.com>
2023-08-07 20:41:15 +1000

    Update callouts.js

--- assets/js/callouts.js
+++ assets/js/callouts.js
@@ -5,7 +5,13 @@ const addCollapsibleCallouts = () => {
   if (collapsibleCallouts.length != 0) {
     console.log(collapsibleCallouts);
     collapsibleCallouts.forEach(el => el.addEventListener('click', event => {
-      event.currentTarget.classList.toggle("callout-collapsed");
+      const $toggledClass = "callout-collapsed";
+      let $before = event.currentTarget.classList.contains($toggledClass);
+      event.currentTarget.classList.toggle($toggledClass);
+      let $after = event.currentTarget.classList.contains($toggledClass);
+      if ($before == $after) {
+        event.currentTarget.classList.toggle($toggledClass);
+      }
     }));
   }
 }
