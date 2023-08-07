ca4130e584b0a1c389e47ea5e8cc37ed0968fd32
Ryan <139601671+Ryan5358@users.noreply.github.com>
2023-08-07 17:51:36 +1000

    Update callouts.js

--- assets/js/callouts.js
+++ assets/js/callouts.js
@@ -4,19 +4,8 @@ const addCollapsibleCallouts = () => {
   
   if (collapsibleCallouts.length != 0) {
     console.log(collapsibleCallouts);
-
-    window.addEventListener('million:navigate', (event) => {
-      console.log("Navigating");
-      console.log(event.detail.url.pathname);
-      console.log("current path");
-      console.log(window.location.pathname);
-      console.log(event.detail.url.pathname != window.location.pathname);
-      if (event.detail.url.pathname != window.location.pathname) {
-        console.log("Adding Collapsible Callouts Event Listener");
-        collapsibleCallouts.forEach(el => el.addEventListener('click', event => {
-          event.currentTarget.classList.toggle("callout-collapsed");
-        }));
-      }
-    });
+    collapsibleCallouts.forEach(el => el.addEventListener('click', event => {
+      event.currentTarget.classList.toggle("callout-collapsed");
+    }));
   }
 }
