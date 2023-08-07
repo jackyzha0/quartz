526e6950363c093de1bf350701b201e25062fa1c
Ryan <139601671+Ryan5358@users.noreply.github.com>
2023-08-08 00:20:03 +1000

    Update callouts.js

--- assets/js/callouts.js
+++ assets/js/callouts.js
@@ -1,13 +1,12 @@
 const addCollapsibleCallouts = () => {
   var initToggle = false;
   const collapsibleCallouts = document.querySelectorAll("blockquote.callout-collapsible");
-  const toggler = fucntion(event) {
-    event.currentTarget.classList.toggle("callout-collapsed");
-  };
   
   if (collapsibleCallouts.length != 0) {
     console.log(collapsibleCallouts);
     var initToggle = true;
-    collapsibleCallouts.forEach(el => el.addEventListener('click', toggler));
+    collapsibleCallouts.forEach(el => el.addEventListener('click', (event) => {
+      event.currentTarget.classList.toggle("callout-collapsed");
+    }));
   }
 }
