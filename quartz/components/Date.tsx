3f8329207b8406bdcaab066f92d29a07b5521857
Jacky Zhao <j.zhao2k19@gmail.com>
2023-08-08 21:28:09 -0700

    change reading time to content meta

--- quartz/components/Date.tsx
+++ quartz/components/Date.tsx
@@ -2,11 +2,14 @@ interface Props {
   date: Date
 }
 
-export function Date({ date }: Props) {
-  const formattedDate = date.toLocaleDateString("en-US", {
+export function formatDate(d: Date): string {
+  return d.toLocaleDateString("en-US", {
     year: "numeric",
     month: "short",
     day: "2-digit",
   })
-  return <>{formattedDate}</>
+}
+
+export function Date({ date }: Props) {
+  return <>{formatDate(date)}</>
 }
