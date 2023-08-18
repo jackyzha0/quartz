d3bf53bb584e8f40a3463e860f4810ba35f11d48
Kai Ma <kai.xykmz@gmail.com>
2023-08-18 00:19:20 -0700

    Quartz sync: Aug 18, 2023, 12:19 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -51,7 +51,7 @@ const config: QuartzConfig = {
       Plugin.SyntaxHighlighting(),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.CrawlLinks({ markdownLinkResolution: "absolute" }),
       Plugin.Latex({ renderEngine: "katex" }),
       Plugin.Description(),
     ],
