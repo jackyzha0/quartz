f6b5a84c7a2325360a69b1e1d0c9636f1925a033
Pelayo Arbues <gonzalezpelayo@gmail.com>
2023-08-22 12:18:34 +0200

    Updates config

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Pelayo Arbués",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "pelayoarbues.github.io",
     ignorePatterns: ["private", "templates"],
     theme: {
       typography: {
