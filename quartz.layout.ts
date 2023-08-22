ced977b983ed2578e47791eeb15265792d4510b9
Pelayo Arbues <gonzalezpelayo@gmail.com>
2023-08-22 12:34:53 +0200

    Adds new config

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,9 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/pelayoarbues",
+      Twitter: "https://www.twitter.com/pelayoarbues",
+      Linkedin: "https://www.linkedin.com/pelayoarbues"
     },
   }),
 }
