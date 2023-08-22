a90455e795a2a0cac89677c6356c0b72cd48c3fa
Pelayo Arbues <gonzalezpelayo@gmail.com>
2023-08-22 15:17:20 +0200

    Adds simpleslug

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,5 +1,6 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import { SimpleSlug } from "./quartz/util/path"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
