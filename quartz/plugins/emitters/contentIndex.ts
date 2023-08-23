2ecc650d42a6adfcfd5143ead5352f14561a9f4b
Jacky Zhao <j.zhao2k19@gmail.com>
2023-08-22 23:33:58 -0700

    fix: percent-encoding for files with %, contentIndex for non-latin chars (closes #397, closes #399)

--- quartz/plugins/emitters/contentIndex.ts
+++ quartz/plugins/emitters/contentIndex.ts
@@ -22,7 +22,7 @@ interface Options {
 const defaultOptions: Options = {
   enableSiteMap: true,
   enableRSS: true,
-  includeEmptyFiles: false,
+  includeEmptyFiles: true,
 }
 
 function generateSiteMap(cfg: GlobalConfiguration, idx: ContentIndex): string {
