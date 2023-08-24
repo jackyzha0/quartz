9707aadf11cf825f00691b7e6732e246d068161c
Jacky Zhao <j.zhao2k19@gmail.com>
2023-08-24 10:03:14 -0700

    format

--- quartz/components/Date.tsx
+++ quartz/components/Date.tsx
@@ -9,7 +9,9 @@ export type ValidDateType = keyof Required<QuartzPluginData>["dates"]
 
 export function getDate(cfg: GlobalConfiguration, data: QuartzPluginData): Date | undefined {
   if (!cfg.defaultDateType) {
-    throw new Error(`Field 'defaultDateType' was not set in the configuration object of quartz.config.ts. See https://quartz.jzhao.xyz/configuration#general-configuration for more details.`)
+    throw new Error(
+      `Field 'defaultDateType' was not set in the configuration object of quartz.config.ts. See https://quartz.jzhao.xyz/configuration#general-configuration for more details.`,
+    )
   }
   return data.dates?.[cfg.defaultDateType]
 }
