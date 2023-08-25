340e3ef5116cd99c8ddfdbb3d9e0bbd914e07825
Ben Schlegel <31989404+benschlegel@users.noreply.github.com>
2023-08-25 18:03:49 +0200

    feat(consistency): Add `.obsidian` to ignorePatterns (#420)

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates"],
+    ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
       typography: {
