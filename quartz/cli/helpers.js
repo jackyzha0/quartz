4b89202f7e834cf8b5c5aa39e8f1778706492085
Ben Schlegel <31989404+benschlegel@users.noreply.github.com>
2023-08-28 00:59:51 +0200

    cleanup: rework cli to allow invoking create and build outside of cli (#428)
    * refactor: move `bootstrap-cli.mjs` tp cli
    
    also update reference in docs
    
    * refactor(cli): move build handler to `cli-functions`
    
    * refactor(cli): move create to handler + helpers
    
    * refactor(cli): extract arg definitions
    
    * refactor: rename handlers and helpers
    
    * refactor(cli): move update, await handlers
    
    * refactor(cli): create constants, migrate to helpers
    
    * refactor(cli): migrate `restore`
    
    * refactor(cli): migrate `sync`
    
    * format
    
    * refactor(cli): remove old imports/functions
    
    * refactor(cli): remove unused imports + format
    
    * chore: remove old log statement
    
    * fix: fix imports, clean duplicate code
    
    * fix: relative import
    
    * fix: simplified cacheFile path
    
    * fix: update cacheFile import path
    
    * refactor: move bootstrap-cli to quartz
    
    * format
    
    * revert: revert path to bootstrap-cli
    
    * ci: re-run
    
    * ci: fix  execution permission

---
+++ quartz/cli/helpers.js
@@ -0,0 +1,52 @@
+import { isCancel, outro } from "@clack/prompts"
+import chalk from "chalk"
+import { contentCacheFolder } from "./constants.js"
+import { spawnSync } from "child_process"
+import fs from "fs"
+
+export function escapePath(fp) {
+  return fp
+    .replace(/\\ /g, " ") // unescape spaces
+    .replace(/^".*"$/, "$1")
+    .replace(/^'.*"$/, "$1")
+    .trim()
+}
+
+export function exitIfCancel(val) {
+  if (isCancel(val)) {
+    outro(chalk.red("Exiting"))
+    process.exit(0)
+  } else {
+    return val
+  }
+}
+
+export async function stashContentFolder(contentFolder) {
+  await fs.promises.rm(contentCacheFolder, { force: true, recursive: true })
+  await fs.promises.cp(contentFolder, contentCacheFolder, {
+    force: true,
+    recursive: true,
+    verbatimSymlinks: true,
+    preserveTimestamps: true,
+  })
+  await fs.promises.rm(contentFolder, { force: true, recursive: true })
+}
+
+export function gitPull(origin, branch) {
+  const flags = ["--no-rebase", "--autostash", "-s", "recursive", "-X", "ours", "--no-edit"]
+  const out = spawnSync("git", ["pull", ...flags, origin, branch], { stdio: "inherit" })
+  if (out.stderr) {
+    throw new Error(`Error while pulling updates: ${out.stderr}`)
+  }
+}
+
+export async function popContentFolder(contentFolder) {
+  await fs.promises.rm(contentFolder, { force: true, recursive: true })
+  await fs.promises.cp(contentCacheFolder, contentFolder, {
+    force: true,
+    recursive: true,
+    verbatimSymlinks: true,
+    preserveTimestamps: true,
+  })
+  await fs.promises.rm(contentCacheFolder, { force: true, recursive: true })
+}
