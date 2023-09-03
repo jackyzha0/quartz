c4e431e877b149031765298ddfc3f6a1d97e4890
Ben Schlegel <ben5.schlegel@gmail.com>
2023-09-03 18:27:00 +0200

    feat: integrate new component/plugin

--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -8,6 +8,7 @@ import PageTitle from "./PageTitle"
 import ContentMeta from "./ContentMeta"
 import Spacer from "./Spacer"
 import TableOfContents from "./TableOfContents"
+import Explorer from "./Explorer"
 import TagList from "./TagList"
 import Graph from "./Graph"
 import Backlinks from "./Backlinks"
@@ -28,6 +29,7 @@ export {
   ContentMeta,
   Spacer,
   TableOfContents,
+  Explorer,
   TagList,
   Graph,
   Backlinks,
