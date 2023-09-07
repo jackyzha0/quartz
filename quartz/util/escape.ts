828aa71fe34aae675a7552957e8a062c82f595f6
Jacky Zhao <j.zhao2k19@gmail.com>
2023-09-06 21:47:59 -0700

    fix: escape encoding for titles in rss

---
+++ quartz/util/escape.ts
@@ -0,0 +1,8 @@
+export const escapeHTML = (unsafe: string) => {
+  return unsafe
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#039;")
+}
