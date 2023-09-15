41bc875e970b5ac0a4d80b4ef235a72d841b7e09
Aura <dykeaura@gmail.com>
2023-09-14 20:03:06 -0600

    change some basic configs for quartz

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "gaylor-wiki.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
