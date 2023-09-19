bf874dc15d41b718cbcc8bdf5f25b7f0ab828a3a
Richard Wutscher <richard.wutscher@gmail.com>
2023-09-19 20:24:35 +0200

    fix: tag regex no longer includes purely numerical 'tags'

--- quartz/plugins/transformers/ofm.ts
+++ quartz/plugins/transformers/ofm.ts
@@ -121,9 +121,11 @@ const calloutRegex = new RegExp(/^\[\!(\w+)\]([+-]?)/)
 const calloutLineRegex = new RegExp(/^> *\[\!\w+\][+-]?.*$/, "gm")
 // (?:^| )              -> non-capturing group, tag should start be separated by a space or be the start of the line
 // #(...)               -> capturing group, tag itself must start with #
-// (?:[-_\p{L}])+       -> non-capturing group, non-empty string of (Unicode-aware) alpha-numeric characters, hyphens and/or underscores
+// (?:[-_\p{L}\d])*     -> non-capturing group, arbitrary number of (Unicode-aware) alpha-numeric characters, hyphens and/or underscores
+// (?:[-_\p{L}])+       -> non-capturing group, non-empty string of (Unicode-aware) letters, hyphens and/or underscores
+// (?:[-_\p{L}\d])*     -> non-capturing group, arbitrary number of (Unicode-aware) alpha-numeric characters, hyphens and/or underscores
 // (?:\/[-_\p{L}]+)*)   -> non-capturing group, matches an arbitrary number of tag strings separated by "/"
-const tagRegex = new RegExp(/(?:^| )#((?:[-_\p{L}\d])+(?:\/[-_\p{L}\d]+)*)/, "gu")
+const tagRegex = new RegExp(/(?:^| )#((?:[-_\p{L}\d])*(?:[-_\p{L}])+(?:[-_\p{L}\d])*(?:\/[-_\p{L}\d]+)*)/, "gu")
 const blockReferenceRegex = new RegExp(/\^([A-Za-z0-9]+)$/, "g")
 
 export const ObsidianFlavoredMarkdown: QuartzTransformerPlugin<Partial<Options> | undefined> = (
