b679d4101f4b37fac44eb13cd6d3b664743b264a
Jacky Zhao <j.zhao2k19@gmail.com>
2023-09-20 13:52:29 -0700

    Revert "docs: wording changes for offline support"
    This reverts commit 09c5d42fa937675acb99fad41a065849f70018f8.

--- quartz/components/pages/OfflineFallbackPage.tsx
+++ quartz/components/pages/OfflineFallbackPage.tsx
@@ -4,7 +4,7 @@ function OfflineFallbackPage() {
   return (
     <article class="popover-hint">
       <h1>Offline</h1>
-      <p>You're offline and this page hasn't been cached yet.</p>
+      <p>This page isn't offline available yet.</p>
     </article>
   )
 }
