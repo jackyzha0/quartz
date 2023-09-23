2569875624526cacf6ec66fe15b31f47553a4c53
Miguel Pimentel <contact@miguelpimentel.do>
2023-09-22 21:33:38 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "https://forgetfulnotes.com/",
+    baseUrl: "forgetfulnotes.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
