4b36fc57ea299d9d9b6b2c02483c06717cb5ee31
Ben Schlegel <31989404+benschlegel@users.noreply.github.com>
2023-09-29 10:26:15 +0200

    feat: implement breadcrumb component (#508)
    * feat: implement breadcrumbs
    
    * style: fix styling, move breadcrumbs to top
    
    * refactor: move `capitalize to `lang.ts``
    
    * refactor: clean breadcrumb generation
    
    * feat: add options to breadcrumbs
    
    * feat: implement `resolveFrontmatterTitle`
    
    * feat: add `hideOnRoot` option
    
    * feat(consistency): capitalize every crumb
    
    * style: add `flex-wrap` to parent container
    
    * refactor: clean `Breadcrumbs.tsx`
    
    * feat(accessibility): use `nav`, add aria label
    
    * style: improve look in popovers by adding margin
    
    * docs: write docs for breadcrumb component
    
    * refactor: collapse `if` condition for hideOnRoot
    
    * chore: add todo for perf optimization
    
    * docs: update introduction

--- quartz/util/lang.ts
+++ quartz/util/lang.ts
@@ -5,3 +5,7 @@ export function pluralize(count: number, s: string): string {
     return `${count} ${s}s`
   }
 }
+
+export function capitalize(s: string): string {
+  return s.substring(0, 1).toUpperCase() + s.substring(1)
+}
