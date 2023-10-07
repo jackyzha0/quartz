581cf9497be8e3f17c086c1e6b3d82b060167c6b
bfahrenfort <bfahrenfort1@gmail.com>
2023-10-07 14:35:08 -0500

    Quartz sync: Oct 7, 2023, 2:35 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -65,6 +65,7 @@ const config: QuartzConfig = {
       Plugin.ContentIndex({
         enableSiteMap: true,
         enableRSS: true,
+        rssLimit: 50,
       }),
       Plugin.Assets(),
       Plugin.Static(),
