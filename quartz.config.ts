d5d9a08c072a29a1205f4a2a5892eb88049102d3
Valentin Ottaviano <valentinottaviano@gmail.com>
2023-10-24 17:34:33 -0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "quartz-alpha.vercel.app",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
