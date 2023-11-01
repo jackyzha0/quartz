f833d4626c163e46a3ab3995f9a32a21bbd5a71c
Milan Chauhan <milanchauhan.development@gmail.com>
2023-11-01 23:20:15 +0530

    Update Page Title

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Milan Chauhan",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
