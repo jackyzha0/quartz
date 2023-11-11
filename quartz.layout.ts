e7b8b1851b5aa8de34a650088da53aad4b876175
jahinzee <jahinzee@outlook.com>
2023-11-11 17:06:40 +1100

    replaced breadcrumbs w/ backlinks as my weird dir struct kinda breaks it

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -16,7 +16,7 @@ export const sharedPageComponents: SharedLayout = {
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     // Component.PageTitle(),
-    Component.Breadcrumbs(),
+    // Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
@@ -27,6 +27,7 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   right: [
     Component.DesktopOnly(Component.TableOfContents()),
+    Component.DesktopOnly(Component.Backlinks())
   ],
 }
 
