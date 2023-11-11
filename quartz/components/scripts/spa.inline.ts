f67356c3d2d609a8a6b674ac650fc41d6dfb4cc7
Jacky Zhao <j.zhao2k19@gmail.com>
2023-11-11 12:02:34 -0800

    lint: format

--- quartz/components/scripts/spa.inline.ts
+++ quartz/components/scripts/spa.inline.ts
@@ -157,7 +157,7 @@ if (!customElements.get("route-announcer")) {
     style:
       "position: absolute; left: 0; top: 0; clip: rect(0 0 0 0); clip-path: inset(50%); overflow: hidden; white-space: nowrap; width: 1px; height: 1px",
   }
-  
+
   customElements.define(
     "route-announcer",
     class RouteAnnouncer extends HTMLElement {
