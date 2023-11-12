cf6ab9e9333b5f76cb9e06f6687f2b4f8fbe91bd
Jacky Zhao <j.zhao2k19@gmail.com>
2023-11-12 14:27:53 -0800

    feat: option to specify npx quartz sync message (closes #583)

--- quartz/cli/handlers.js
+++ quartz/cli/handlers.js
@@ -483,8 +483,9 @@ export async function handleSync(argv) {
       dateStyle: "medium",
       timeStyle: "short",
     })
+    const commitMessage = argv.message ?? `Quartz sync: ${currentTimestamp}`
     spawnSync("git", ["add", "."], { stdio: "inherit" })
-    spawnSync("git", ["commit", "-m", `Quartz sync: ${currentTimestamp}`], { stdio: "inherit" })
+    spawnSync("git", ["commit", "-m", commitMessage], { stdio: "inherit" })
 
     if (contentStat.isSymbolicLink()) {
       // put symlink back
