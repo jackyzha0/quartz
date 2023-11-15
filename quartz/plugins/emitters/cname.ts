9e879dc8dfe24c47e39ef00c736c14fa72990151
Zijing Zhang <50045289+pluveto@users.noreply.github.com>
2023-11-16 07:19:52 +0800

    Update cname.ts

--- quartz/plugins/emitters/cname.ts
+++ quartz/plugins/emitters/cname.ts
@@ -1,18 +1,28 @@
 import { FilePath, QUARTZ, joinSegments } from "../../util/path"
 import { QuartzEmitterPlugin } from "../types"
 import fs from "fs"
-export const Cname: QuartzEmitterPlugin = () => ({
-  name: "Cname",
+
+export function extractDomainFromBaseUrl(baseUrl: string) {
+  const url = new URL(`https://${baseUrl}`)
+  return url.hostname
+}
+
+export const CNAME: QuartzEmitterPlugin = () => ({
+  name: "CNAME",
   getQuartzComponents() {
     return []
   },
   async emit({ argv, cfg }, _content, _resources, _emit): Promise<FilePath[]> {
+    if(!cfg.configuration.baseUrl) {
+      console.warn("No baseUrl set while CNAME emitter enabled.")
+      return []
+    }
     const path = joinSegments(argv.output, "CNAME")
-    const content = cfg.configuration.baseUrl
+    const content = extractDomainFromBaseUrl(cfg.configuration.baseUrl)
     if (!content) {
       return []
     }
     fs.writeFileSync(path, content)
-    return [joinSegments(argv.output, "CNAME")] as FilePath[]
+    return [path] as FilePath[]
   },
 })
