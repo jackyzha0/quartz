2d89e2bcc128c017649e1a63249a438e2fb00923
Zijing Zhang <50045289+pluveto@users.noreply.github.com>
2023-11-16 14:03:06 +0800

    Update cname.ts

--- quartz/plugins/emitters/cname.ts
+++ quartz/plugins/emitters/cname.ts
@@ -1,6 +1,7 @@
 import { FilePath, joinSegments } from "../../util/path"
 import { QuartzEmitterPlugin } from "../types"
 import fs from "fs"
+import chalk from "chalk"
 
 export function extractDomainFromBaseUrl(baseUrl: string) {
   const url = new URL(`https://${baseUrl}`)
@@ -14,7 +15,7 @@ export const CNAME: QuartzEmitterPlugin = () => ({
   },
   async emit({ argv, cfg }, _content, _resources, _emit): Promise<FilePath[]> {
     if (!cfg.configuration.baseUrl) {
-      console.warn("No baseUrl set while CNAME emitter enabled.")
+      console.warn(chalk.yellow("CNAME emitter requires \`baseUrl\` to be set in your configuration"))
       return []
     }
     const path = joinSegments(argv.output, "CNAME")
