68d9eef33690af1ac4c4eb100e50f9d7b7174957
vintro <vince@plasticlabs.ai>
2023-12-12 15:19:10 -0500

    add package lock

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,6 +11,7 @@ const config: QuartzConfig = {
     },
     baseUrl: "plasticlabs.ai",
     ignorePatterns: ["private", "templates"],
+    defaultDateType: "created",
     theme: {
       typography: {
         header: "Spectral",
