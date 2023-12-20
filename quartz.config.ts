1d49bb1086a87a8a2acc30e1bf9db196cc1e864a
Erdem Özgen <m.erdemozgen@gmail.com>
2023-12-20 09:36:20 +0300

    change urls

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "erdemozgen.github.io/brain",
+    baseUrl: "erdemozgen.github.io/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,7 +7,7 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
+      GitHub: "https://github.com/ErdemOzgen/erdemozgen.github.io",
       "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
