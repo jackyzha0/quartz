a3e9340f8e1a8b1f501df2c74003ef58488b1fff
LUCASTUCIOUS <peterlucas2804@gmail.com>
2024-01-19 15:41:21 +0100

    Add cssclasses to article

--- quartz/components/pages/Content.tsx
+++ quartz/components/pages/Content.tsx
@@ -1,9 +1,14 @@
 import { htmlToJsx } from "../../util/jsx"
 import { QuartzComponentConstructor, QuartzComponentProps } from "../types"
+import assert from 'node:assert';
 
 function Content({ fileData, tree }: QuartzComponentProps) {
-  const content = htmlToJsx(fileData.filePath!, tree)
-  return <article class="popover-hint">{content}</article>
+  const content = htmlToJsx(fileData.filePath!, tree);
+  let CSSclass: string[] = fileData.frontmatter?.cssclasses || [];
+  CSSclass.push("popover-hint");
+  const finalClass = CSSclass.join(" ");
+  //const  = "popover-hint"+fileData.frontmatter?.cssclasses
+  return <article class={finalClass}>{content}</article>
 }
 
 export default (() => Content) satisfies QuartzComponentConstructor
