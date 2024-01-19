46901eef8e054feaeb80ee44bfaf9d2260d609f5
LUCASTUCIOUS <peterlucas2804@gmail.com>
2024-01-19 15:55:39 +0100

    Prettier

--- quartz/components/pages/Content.tsx
+++ quartz/components/pages/Content.tsx
@@ -1,12 +1,12 @@
 import { htmlToJsx } from "../../util/jsx"
 import { QuartzComponentConstructor, QuartzComponentProps } from "../types"
-import assert from 'node:assert';
+import assert from "node:assert"
 
 function Content({ fileData, tree }: QuartzComponentProps) {
-  const content = htmlToJsx(fileData.filePath!, tree);
-  let CSSclass: string[] = fileData.frontmatter?.cssclasses || [];
-  CSSclass.push("popover-hint");
-  const finalClass = CSSclass.join(" ");
+  const content = htmlToJsx(fileData.filePath!, tree)
+  let CSSclass: string[] = fileData.frontmatter?.cssclasses || []
+  CSSclass.push("popover-hint")
+  const finalClass = CSSclass.join(" ")
   //const  = "popover-hint"+fileData.frontmatter?.cssclasses
   return <article class={finalClass}>{content}</article>
 }
