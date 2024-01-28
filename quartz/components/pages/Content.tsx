004b28d61053fdb74611ae750454227fab35eddd
Jacky Zhao <j.zhao2k19@gmail.com>
2024-01-27 18:29:46 -0800

    Update quartz/components/pages/Content.tsx

--- quartz/components/pages/Content.tsx
+++ quartz/components/pages/Content.tsx
@@ -1,6 +1,5 @@
 import { htmlToJsx } from "../../util/jsx"
 import { QuartzComponentConstructor, QuartzComponentProps } from "../types"
-import assert from "node:assert"
 
 function Content({ fileData, tree }: QuartzComponentProps) {
   const content = htmlToJsx(fileData.filePath!, tree)
