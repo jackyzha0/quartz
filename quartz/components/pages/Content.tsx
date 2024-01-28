1d500e12b54fdcb114ee8695824f09bcec81f38c
Jacky Zhao <j.zhao2k19@gmail.com>
2024-01-27 18:29:41 -0800

    Update quartz/components/pages/Content.tsx

--- quartz/components/pages/Content.tsx
+++ quartz/components/pages/Content.tsx
@@ -4,11 +4,9 @@ import assert from "node:assert"
 
 function Content({ fileData, tree }: QuartzComponentProps) {
   const content = htmlToJsx(fileData.filePath!, tree)
-  let CSSclass: string[] = fileData.frontmatter?.cssclasses || []
-  CSSclass.push("popover-hint")
-  const finalClass = CSSclass.join(" ")
-  //const  = "popover-hint"+fileData.frontmatter?.cssclasses
-  return <article class={finalClass}>{content}</article>
+  const classes: string[] = fileData.frontmatter?.cssclasses ?? []
+  const classString = ["popover-hint", ...classes].join(" ")
+  return <article class={classString}>{content}</article>
 }
 
 export default (() => Content) satisfies QuartzComponentConstructor
