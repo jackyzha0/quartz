ab3e1c8f0b2a184932d8039117bf98d354c71c87
Mara <mara-li@outlook.fr>
2024-01-29 08:09:08 +0100

    forgot to remove fr-FR testing

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "fr-FR",
+    locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
