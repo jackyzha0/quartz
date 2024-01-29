156f747c4acbeaa7c0e8fb89cf10bb83a11a4a2d
Aaron <29749331+aarnphm@users.noreply.github.com>
2024-01-29 01:54:00 -0500

    feat(icon): update content for gfm links
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- quartz/plugins/transformers/gfm.ts
+++ quartz/plugins/transformers/gfm.ts
@@ -14,6 +14,8 @@ const defaultOptions: Options = {
   linkHeadings: true,
 }
 
+const linkIcon = `<svg xmlns="http://www.w3.org/2000/svg><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></svg>`
+
 export const GitHubFlavoredMarkdown: QuartzTransformerPlugin<Partial<Options> | undefined> = (
   userOpts,
 ) => {
@@ -37,8 +39,36 @@ export const GitHubFlavoredMarkdown: QuartzTransformerPlugin<Partial<Options> | 
                 "data-no-popover": true,
               },
               content: {
-                type: "text",
-                value: " §",
+                type: "element",
+                tagName: "svg",
+                properties: {
+                  width: 18,
+                  height: 18,
+                  viewBox: "0 0 24 24",
+                  fill: "none",
+                  stroke: "currentColor",
+                  "stroke-width": "2",
+                  "stroke-linecap": "round",
+                  "stroke-linejoin": "round",
+                },
+                children: [
+                  {
+                    type: "element",
+                    tagName: "path",
+                    properties: {
+                      d: "M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71",
+                    },
+                    children: [],
+                  },
+                  {
+                    type: "element",
+                    tagName: "path",
+                    properties: {
+                      d: "M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71",
+                    },
+                    children: [],
+                  },
+                ],
               },
             },
           ],
