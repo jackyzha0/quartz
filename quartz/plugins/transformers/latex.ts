fee3ef9b3a0135fd98c412f50609b0d1009ff55c
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2024-01-31 12:25:16 -0500

    chore(deps): bump katex to 0.16.9 (#772)
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- quartz/plugins/transformers/latex.ts
+++ quartz/plugins/transformers/latex.ts
@@ -26,12 +26,12 @@ export const Latex: QuartzTransformerPlugin<Options> = (opts?: Options) => {
         return {
           css: [
             // base css
-            "https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css",
+            "https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css",
           ],
           js: [
             {
               // fix copy behaviour: https://github.com/KaTeX/KaTeX/blob/main/contrib/copy-tex/README.md
-              src: "https://cdn.jsdelivr.net/npm/katex@0.16.7/dist/contrib/copy-tex.min.js",
+              src: "https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/contrib/copy-tex.min.js",
               loadTime: "afterDOMReady",
               contentType: "external",
             },
