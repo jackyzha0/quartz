1a2b98e956690db3ce08baa68ffccdda0f218876
Cody <CodyEthanJordan@users.noreply.github.com>
2024-02-04 03:27:03 -0600

    Dont show reading time

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -11,7 +11,7 @@ interface ContentMetaOptions {
 }
 
 const defaultOptions: ContentMetaOptions = {
-  showReadingTime: true,
+  showReadingTime: false,
 }
 
 export default ((opts?: Partial<ContentMetaOptions>) => {
