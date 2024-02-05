b061b1b6a229e042a2a0c08e5a4d8ed016f46a1e
Mats Fangohr <83273529+MatsFangohr@users.noreply.github.com>
2024-02-05 15:59:58 +0100

    feat(i18n): German translation (#808)

--- quartz/i18n/index.ts
+++ quartz/i18n/index.ts
@@ -1,10 +1,12 @@
 import { Translation } from "./locales/definition"
 import en from "./locales/en-US"
 import fr from "./locales/fr-FR"
+import de from "./locales/de-DE"
 
 export const TRANSLATIONS = {
   "en-US": en,
   "fr-FR": fr,
+  "de-DE": de,
 } as const
 
 export const i18n = (locale: ValidLocale): Translation => TRANSLATIONS[locale]
