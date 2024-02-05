95ff7ea87d9ae5ece606b57778dc3fb21c5506ff
Tomoya Matsuura(MacBookPro) <me@matsuuratomoya.com>
2024-02-05 23:25:28 +0900

    format

--- quartz/i18n/index.ts
+++ quartz/i18n/index.ts
@@ -1,10 +1,12 @@
 import { Translation } from "./locales/definition"
 import en from "./locales/en-US"
 import fr from "./locales/fr-FR"
+import ja from "./locales/ja-JP"
 
 export const TRANSLATIONS = {
   "en-US": en,
   "fr-FR": fr,
+  "ja-JP": ja,
 } as const
 
 export const i18n = (locale: ValidLocale): Translation => TRANSLATIONS[locale]
