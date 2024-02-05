b169a5880fd346d5c23154eba50f29b2c01f0eb9
Emile Bangma <ewjbangma@hotmail.com>
2024-02-05 22:12:54 +0100

    feat(i18n): Add Dutch to i18n (#813)
    * Create nl-NL.ts
    
    * Update index.ts
    
    * Update nl-NL.ts

--- quartz/i18n/index.ts
+++ quartz/i18n/index.ts
@@ -3,12 +3,14 @@ import en from "./locales/en-US"
 import fr from "./locales/fr-FR"
 import ja from "./locales/ja-JP"
 import de from "./locales/de-DE"
+import nl from "./locales/nl-NL"
 
 export const TRANSLATIONS = {
   "en-US": en,
   "fr-FR": fr,
   "ja-JP": ja,
   "de-DE": de,
+  "nl-NL": nl,
 } as const
 
 export const i18n = (locale: ValidLocale): Translation => TRANSLATIONS[locale]
