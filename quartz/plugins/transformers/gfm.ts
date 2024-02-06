8b7caff4730c139f4d8205d562f509cb5373a4b9
Aaron <29749331+aarnphm@users.noreply.github.com>
2024-02-06 02:04:10 -0500

    chore: unify search inner container
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- quartz/plugins/transformers/gfm.ts
+++ quartz/plugins/transformers/gfm.ts
@@ -36,7 +36,6 @@ export const GitHubFlavoredMarkdown: QuartzTransformerPlugin<Partial<Options> | 
                 ariaHidden: true,
                 tabIndex: -1,
                 "data-no-popover": true,
-                "data-icon": true,
               },
               content: {
                 type: "element",
