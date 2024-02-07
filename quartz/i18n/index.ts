ca284778b257698457116d65bc9559dbb37a1166
Miguel Pimentel <contact@miguelpimentel.do>
2024-02-07 09:57:14 -0800

    add Spanish translations (#822)
    * add Spanish translations
    
    * format with prettier
    
    * clears npm ci, formatted w/ prettier

--- quartz/i18n/index.ts
+++ quartz/i18n/index.ts
@@ -5,6 +5,7 @@ import ja from "./locales/ja-JP"
 import de from "./locales/de-DE"
 import nl from "./locales/nl-NL"
 import ro from "./locales/ro-RO"
+import es from "./locales/es-ES"
 
 export const TRANSLATIONS = {
   "en-US": en,
@@ -13,6 +14,7 @@ export const TRANSLATIONS = {
   "de-DE": de,
   "nl-NL": nl,
   "ro-RO": ro,
+  "es-ES": es,
 } as const
 
 export const i18n = (locale: ValidLocale): Translation => TRANSLATIONS[locale ?? "en-US"]
