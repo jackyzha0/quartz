3e3ec58cefdb5eb07257db1e6bf2d0a22baa03c2
Aaron <29749331+aarnphm@users.noreply.github.com>
2024-02-07 07:00:46 -0500

    feat(lang): Set correct HTML lang element based on frontmatter or default locale
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -209,8 +209,10 @@ export function renderPage(
     </div>
   )
 
+  const lang = componentData.frontmatter?.lang ?? cfg.locale.split("-")[0]
+
   const doc = (
-    <html>
+    <html lang={lang}>
       <Head {...componentData} />
       <body data-slug={slug}>
         <div id="quartz-root" class="page">
