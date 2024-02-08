07d6b101162e62583402880df8acaf265d0efabe
jorgegomez <jorgegomez@bmat.com>
2024-02-08 04:28:50 +0100

    feat: added PropertyLinks text transformer

--- quartz.config.ts
+++ quartz.config.ts
@@ -58,6 +58,7 @@ const config: QuartzConfig = {
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
+      Plugin.PropertyLinks(),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
