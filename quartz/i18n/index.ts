ab9ab9e3d668e62048a4d3d95b411fd8ae0bc330
ahmedelq <ahmed.elq53@gmail.com>
2024-02-08 11:37:58 +0300

    feat(i18n): add Arabic translation

--- quartz/i18n/index.ts
+++ quartz/i18n/index.ts
@@ -6,6 +6,7 @@ import de from "./locales/de-DE"
 import nl from "./locales/nl-NL"
 import ro from "./locales/ro-RO"
 import es from "./locales/es-ES"
+import ar from "./locales/ar-SA"
 
 export const TRANSLATIONS = {
   "en-US": en,
@@ -15,6 +16,26 @@ export const TRANSLATIONS = {
   "nl-NL": nl,
   "ro-RO": ro,
   "es-ES": es,
+  "ar-SA": ar,
+  "ar-AE": ar,
+  "ar-QA": ar,
+  "ar-BH": ar,
+  "ar-KW": ar,
+  "ar-OM": ar,
+  "ar-YE": ar,
+  "ar-IR": ar,
+  "ar-SY": ar,
+  "ar-IQ": ar,
+  "ar-JO": ar,
+  "ar-PL": ar,
+  "ar-LB": ar,
+  "ar-EG": ar,
+  "ar-SD": ar,
+  "ar-LY": ar,
+  "ar-MA": ar,
+  "ar-TN": ar,
+  "ar-DZ": ar,
+  "ar-MR": ar,
 } as const
 
 export const i18n = (locale: ValidLocale): Translation => TRANSLATIONS[locale ?? "en-US"]
