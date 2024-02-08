e01f3a202f36dcf5c9508c063048cf290a8c86d4
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2024-02-08 02:48:03 -0500

    revert: changes in theme.ts

--- quartz/util/theme.ts
+++ quartz/util/theme.ts
@@ -27,9 +27,7 @@ const DEFAULT_SANS_SERIF =
 const DEFAULT_MONO = "ui-monospace, SFMono-Regular, SF Mono, Menlo, monospace"
 
 export function googleFontHref(theme: Theme) {
-  const { code, header, body } = Object.fromEntries(
-    Object.entries(theme.typography).map(([key, value]) => [key, value.replace(/ /g, "+")]),
-  )
+  const { code, header, body } = theme.typography
   return `https://fonts.googleapis.com/css2?family=${code}&family=${header}:wght@400;700&family=${body}:ital,wght@0,400;0,600;1,400;1,600&display=swap`
 }
 
