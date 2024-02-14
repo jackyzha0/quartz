b80f192c68e7186ee99c84b98d13fa72df73a785
Silviu Lorenț <124451350+smilorent@users.noreply.github.com>
2024-02-14 21:49:41 +0200

    feat: add user-defined options to syntax highlighting plugin

--- quartz/plugins/transformers/syntax.ts
+++ quartz/plugins/transformers/syntax.ts
@@ -1,20 +1,41 @@
 import { QuartzTransformerPlugin } from "../types"
 import rehypePrettyCode, { Options as CodeOptions } from "rehype-pretty-code"
+import { BuiltinTheme } from "shikiji"
 
-export const SyntaxHighlighting: QuartzTransformerPlugin = () => ({
-  name: "SyntaxHighlighting",
-  htmlPlugins() {
-    return [
-      [
-        rehypePrettyCode,
-        {
-          keepBackground: false,
-          theme: {
-            dark: "github-dark",
-            light: "github-light",
-          },
-        } satisfies Partial<CodeOptions>,
-      ],
-    ]
+interface Theme {
+  light?: BuiltinTheme
+  dark?: BuiltinTheme
+}
+
+interface Options {
+  theme?: Theme
+  keepBackground?: boolean
+}
+
+const defaultOptions: Options = {
+  theme: {
+    light: "github-light",
+    dark: "github-dark",
   },
-})
+  keepBackground: false,
+}
+
+export const SyntaxHighlighting: QuartzTransformerPlugin<Options> = (opts?: Partial<Options>) => {
+  const { theme = defaultOptions.theme || {}, keepBackground } = { ...defaultOptions, ...opts }
+  const { light = defaultOptions.theme?.light, dark = defaultOptions.theme?.dark } = theme
+
+  return {
+    name: "SyntaxHighlighting",
+    htmlPlugins() {
+      return [
+        [
+          rehypePrettyCode,
+          {
+            theme: { light, dark },
+            keepBackground,
+          } as Partial<CodeOptions>,
+        ],
+      ]
+    },
+  }
+}
