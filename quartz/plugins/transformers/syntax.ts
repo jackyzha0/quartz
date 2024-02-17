fa2ea2896f0977253733334199d28e509351e621
Silviu Lorenț <124451350+smilorent@users.noreply.github.com>
2024-02-17 20:23:45 +0200

    feat: add user-defined config for syntax highlighting plugin (#869)
    * feat: add user-defined options to syntax highlighting plugin
    
    * feat: add default syntax highlighting config to `quartz.config.ts`
    
    * chore: refactor according to @aarnphm's review
    
    Co-authored-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>
    
    * chore: run Prettier on `quartz/plugins/transformers/syntax.ts`
    
    * Update quartz/plugins/transformers/syntax.ts
    
    Co-authored-by: Jacky Zhao <j.zhao2k19@gmail.com>
    
    * Update syntax.ts
    
    ---------
    
    Co-authored-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>
    Co-authored-by: Jacky Zhao <j.zhao2k19@gmail.com>

--- quartz/plugins/transformers/syntax.ts
+++ quartz/plugins/transformers/syntax.ts
@@ -1,20 +1,33 @@
 import { QuartzTransformerPlugin } from "../types"
-import rehypePrettyCode, { Options as CodeOptions } from "rehype-pretty-code"
+import rehypePrettyCode, { Options as CodeOptions, Theme as CodeTheme } from "rehype-pretty-code"
 
-export const SyntaxHighlighting: QuartzTransformerPlugin = () => ({
-  name: "SyntaxHighlighting",
-  htmlPlugins() {
-    return [
-      [
-        rehypePrettyCode,
-        {
-          keepBackground: false,
-          theme: {
-            dark: "github-dark",
-            light: "github-light",
-          },
-        } satisfies Partial<CodeOptions>,
-      ],
-    ]
+interface Theme extends Record<string, CodeTheme> {
+  light: CodeTheme
+  dark: CodeTheme
+}
+
+interface Options {
+  theme?: Theme
+  keepBackground?: boolean
+}
+
+const defaultOptions: Options = {
+  theme: {
+    light: "github-light",
+    dark: "github-dark",
   },
-})
+  keepBackground: false,
+}
+
+export const SyntaxHighlighting: QuartzTransformerPlugin<Options> = (
+  userOpts?: Partial<Options>,
+) => {
+  const opts: Partial<CodeOptions> = { ...defaultOptions, ...userOpts }
+
+  return {
+    name: "SyntaxHighlighting",
+    htmlPlugins() {
+      return [[rehypePrettyCode, opts]]
+    },
+  }
+}
