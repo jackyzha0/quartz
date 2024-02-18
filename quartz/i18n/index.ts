b1a105371bffdea6ca10a010292248ef9aff3ce2
JONG HWAN KIM <99215801+JongDeug@users.noreply.github.com>
2024-02-19 07:37:59 +0900

    feat(i18n): add Korean (#889)
    * feat(i18n): add Korean
    
    * feat(i18n): add Korean

--- quartz/i18n/index.ts
+++ quartz/i18n/index.ts
@@ -9,6 +9,7 @@ import es from "./locales/es-ES"
 import ar from "./locales/ar-SA"
 import uk from "./locales/uk-UA"
 import ru from "./locales/ru-RU"
+import ko from "./locales/ko-KR"
 
 export const TRANSLATIONS = {
   "en-US": en,
@@ -42,6 +43,7 @@ export const TRANSLATIONS = {
   "ar-MR": ar,
   "uk-UA": uk,
   "ru-RU": ru,
+  "ko-KR": ko,
 } as const
 
 export const defaultTranslation = "en-US"
