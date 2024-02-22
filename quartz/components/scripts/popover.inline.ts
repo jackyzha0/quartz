2282265d7389adadb40ecbc78156a7107c70a516
Aaron <29749331+aarnphm@users.noreply.github.com>
2024-02-22 09:45:49 -0500

    feat(popover): add support for PDF
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- quartz/components/scripts/popover.inline.ts
+++ quartz/components/scripts/popover.inline.ts
@@ -48,7 +48,8 @@ async function mouseEnterHandler(
 
   if (!response) return
   const contentType = response.headers.get("Content-Type")
-  const contentTypeCategory = contentType?.split("/")[0] ?? "text"
+  const [contentTypeCategory, typeInfo] = contentType?.split("/") ?? ["text", "html"]
+  const appType = typeInfo.split(";")[0] ?? "html"
 
   const popoverElement = document.createElement("div")
   popoverElement.classList.add("popover")
@@ -56,7 +57,7 @@ async function mouseEnterHandler(
   popoverInner.classList.add("popover-inner")
   popoverElement.appendChild(popoverInner)
 
-  popoverInner.dataset.contentType = contentTypeCategory
+  popoverInner.dataset.contentType = contentType ?? undefined
 
   switch (contentTypeCategory) {
     case "image":
@@ -68,6 +69,17 @@ async function mouseEnterHandler(
       img.alt = targetUrl.pathname
 
       popoverInner.appendChild(img)
+      break
+    case "application":
+      switch (appType) {
+        case "pdf":
+          const pdf = document.createElement("iframe")
+          pdf.src = targetUrl.toString()
+          popoverInner.appendChild(pdf)
+          break
+        default:
+          break
+      }
       break
     default:
       const contents = await response.text()
