0fabfb8bf569e1c26deddc0b017901e4ec452f46
wych <wychwitchcraft@gmail.com>
2024-02-24 20:46:40 -0600

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -27,14 +27,14 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          light: "#fbfcfd",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          secondary: "#96a7df",
+          tertiary: "#e79e73",
+          highlight: "rgba(219, 150, 109, 0.25)",
         },
         darkMode: {
           light: "#161618",
