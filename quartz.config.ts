f54082a347af04eb48f87492a4ad93ee7b3ae8f4
Adam <github@justatom.dev>
2024-02-25 18:53:18 -0500

    Title change

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🧑🏻‍💻 Adam's Place",
+    pageTitle: "🧑🏻‍💻 Adam's Docs",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
