276aca4454b47aedb9acf91d0537793c51071841
Adam Jackson <github@justatom.dev>
2024-02-25 15:43:48 -0500

    Update

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Adam's Place",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
