0c9851b17b233df9931353bdd097fc43d0797886
Lisandra-dev <mara-li@outlook.fr>
2024-02-25 14:52:17 +0000

    refactor: fix style

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -1,29 +1,28 @@
-import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
-import { classNames } from "../util/lang"
-import { IconFolderOptions } from "../plugins/components/FileIcons"
-import { FileTitleIcon } from "../plugins/components/FileIcons"
+import { FileTitleIcon,IconFolderOptions  } from "../plugins/components/FileIcons";
+import { classNames } from "../util/lang";
+import { QuartzComponentConstructor, QuartzComponentProps } from "./types";
 
 export default ((opts?: Partial<IconFolderOptions>) => {
-  function ArticleTitle(props: QuartzComponentProps) {
-    const { displayClass, fileData } = props
-    const title = fileData.frontmatter?.title
-    const iconType = (fileData.frontmatter?.icon as string) || opts?.default?.file
-    if (title) {
-      if (!opts?.rootIconFolder || !iconType) {
-        return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
-      }
-      return (
-        <FileTitleIcon displayClass={displayClass} opts={opts} iconType={iconType} title={title} />
-      )
-    } else {
-      return null
-    }
-  }
+	function ArticleTitle(props: QuartzComponentProps) {
+		const { displayClass, fileData } = props;
+		const title = fileData.frontmatter?.title;
+		const iconType = (fileData.frontmatter?.icon as string) || opts?.default?.file;
+		if (title) {
+			if (!opts?.rootIconFolder || !iconType) {
+				return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>;
+			}
+			return (
+				<FileTitleIcon displayClass={displayClass} opts={opts} iconType={iconType} title={title} />
+			);
+		} else {
+			return null;
+		}
+	}
 
-  ArticleTitle.css = `
+	ArticleTitle.css = `
   .article-title {
     margin: 2rem 0 0 0;
   }
-  `
-  return ArticleTitle
-}) satisfies QuartzComponentConstructor
+  `;
+	return ArticleTitle;
+}) satisfies QuartzComponentConstructor;
