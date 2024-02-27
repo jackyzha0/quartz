9379c4f403e9471756612ac1756edcf0988f0d12
PinkR1ver <3180102330@zju.edu.cn>
2024-02-27 19:48:42 +0800

    Change Configure

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🎣 JudeW's Knowledge Brain",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://pinktalk.online/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
