3863a9839f3955242dee40d77d4fd46b42b6bb92
Emile Bangma <ewjbangma@hotmail.com>
2024-02-29 10:32:13 +0000

    Replace external links with domain name.

--- quartz/plugins/transformers/description.ts
+++ quartz/plugins/transformers/description.ts
@@ -5,10 +5,12 @@ import { escapeHTML } from "../../util/escape"
 
 export interface Options {
   descriptionLength: number
+  removeExternalLinks: boolean
 }
 
 const defaultOptions: Options = {
   descriptionLength: 150,
+  removeExternalLinks: true,
 }
 
 export const Description: QuartzTransformerPlugin<Partial<Options> | undefined> = (userOpts) => {
@@ -21,6 +23,17 @@ export const Description: QuartzTransformerPlugin<Partial<Options> | undefined> 
           return async (tree: HTMLRoot, file) => {
             const frontMatterDescription = file.data.frontmatter?.description
             const text = escapeHTML(toString(tree))
+
+            if (opts.removeExternalLinks) {
+              frontMatterDescription?.replace(
+                /(?:https?:\/\/)?([\da-z\.-]+)\.(?:[a-z\.]{2,6})(?::\d+)?(?:[\/\w\.-]*)/g,
+                "$1",
+              )
+              text.replace(
+                /(?:https?:\/\/)?([\da-z\.-]+)\.(?:[a-z\.]{2,6})(?::\d+)?(?:[\/\w\.-]*)/g,
+                "$1",
+              )
+            }
 
             const desc = frontMatterDescription ?? text
             const sentences = desc.replace(/\s+/g, " ").split(/\.\s/)
