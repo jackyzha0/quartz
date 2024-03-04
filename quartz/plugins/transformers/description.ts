bcb5b2df09cb8c8fc0736ec476b2486f9b4643be
Emile Bangma <ewjbangma@hotmail.com>
2024-03-04 18:52:28 +0100

    feat(frontmatter): configure max length for description (#946)
    * Sentence length check
    
    * Replace external links with domain name.
    
    * Updated documentation.
    
    * Updated replacement values.
    
    * Updated Regex based on feedback.
    
    * Check description for undefined
    
    * Updated external url transform regex.
    
    * Updated formatting

--- quartz/plugins/transformers/description.ts
+++ quartz/plugins/transformers/description.ts
@@ -5,11 +5,18 @@ import { escapeHTML } from "../../util/escape"
 
 export interface Options {
   descriptionLength: number
+  replaceExternalLinks: boolean
 }
 
 const defaultOptions: Options = {
   descriptionLength: 150,
+  replaceExternalLinks: true,
 }
+
+const urlRegex = new RegExp(
+  /(https?:\/\/)?(?<domain>([\da-z\.-]+)\.([a-z\.]{2,6})(:\d+)?)(?<path>[\/\w\.-]*)(\?[\/\w\.=&;-]*)?/,
+  "g",
+)
 
 export const Description: QuartzTransformerPlugin<Partial<Options> | undefined> = (userOpts) => {
   const opts = { ...defaultOptions, ...userOpts }
@@ -19,19 +26,39 @@ export const Description: QuartzTransformerPlugin<Partial<Options> | undefined> 
       return [
         () => {
           return async (tree: HTMLRoot, file) => {
-            const frontMatterDescription = file.data.frontmatter?.description
-            const text = escapeHTML(toString(tree))
+            let frontMatterDescription = file.data.frontmatter?.description
+            let text = escapeHTML(toString(tree))
+
+            if (opts.replaceExternalLinks) {
+              frontMatterDescription = frontMatterDescription?.replace(
+                urlRegex,
+                "$<domain>" + "$<path>",
+              )
+              text = text.replace(urlRegex, "$<domain>" + "$<path>")
+            }
 
             const desc = frontMatterDescription ?? text
-            const sentences = desc.replace(/\s+/g, " ").split(".")
+            const sentences = desc.replace(/\s+/g, " ").split(/\.\s/)
             let finalDesc = ""
             let sentenceIdx = 0
             const len = opts.descriptionLength
-            while (finalDesc.length < len) {
-              const sentence = sentences[sentenceIdx]
-              if (!sentence) break
-              finalDesc += sentence + "."
-              sentenceIdx++
+
+            if (sentences[0] !== undefined && sentences[0].length >= len) {
+              const firstSentence = sentences[0].split(" ")
+              while (finalDesc.length < len) {
+                const sentence = firstSentence[sentenceIdx]
+                if (!sentence) break
+                finalDesc += sentence + " "
+                sentenceIdx++
+              }
+              finalDesc = finalDesc.trimEnd() + "..."
+            } else {
+              while (finalDesc.length < len) {
+                const sentence = sentences[sentenceIdx]
+                if (!sentence) break
+                finalDesc += sentence.endsWith(".") ? sentence : sentence + "."
+                sentenceIdx++
+              }
             }
 
             file.data.description = finalDesc
