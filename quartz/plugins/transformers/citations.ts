19b61088ce8a51ae5ba7b29df815e2735779d3c8
linozen <linus@sehn.dev>
2024-03-12 08:42:48 +0100

    feat: add csl option and add no-popover to citation links

--- quartz/plugins/transformers/citations.ts
+++ quartz/plugins/transformers/citations.ts
@@ -1,14 +1,19 @@
 import rehypeCitation from "rehype-citation"
+import rehypeRewrite from "rehype-rewrite"
 import { QuartzTransformerPlugin } from "../types"
 
 export interface Options {
   bibliographyFile: string
   suppressBibliography: boolean
+  linkCitations: boolean
+  csl: string
 }
 
 const defaultOptions: Options = {
   bibliographyFile: "./bibliography.bib",
   suppressBibliography: false,
+  linkCitations: false,
+  csl: "apa",
 }
 
 export const Citations: QuartzTransformerPlugin<Partial<Options> | undefined> = (userOpts) => {
@@ -22,9 +27,20 @@ export const Citations: QuartzTransformerPlugin<Partial<Options> | undefined> = 
           {
             bibliography: opts.bibliographyFile,
             suppressBibliography: opts.suppressBibliography,
-            // Until popovers are implemented to not show for citation links, I
-            // don't want to link citations
-            linkCitations: false,
+            linkCitations: opts.linkCitations,
+          },
+        ],
+        // Transform the HTML of the citattions; add data-no-popover property to the citation links
+        // using https://github.com/jaywcjlove/rehype-rewrite as they're just anochor links
+        [
+          rehypeRewrite,
+          {
+            rewrite: (node: any) => {
+              // Add data-no-popover to all elements which have an ID that starts with "#bib"
+              if (node.tagName === "a" && node.properties.href?.startsWith("#bib")) {
+                node.properties["data-no-popover"] = true
+              }
+            },
           },
         ],
       ]
