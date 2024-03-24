6efc4dd724870c9e4fbc2e17651cc2a4f943c4a7
Kun-Szabó Kristóf <kunszabokristof.notifications@gmail.com>
2024-03-24 17:35:07 +0100

    i18n: add Hungarian translations (#1023)
    * add Hungarian translations
    
    * typo: _ instead of - in hu-HU
    
    * run prettier
    
    * revert prettier messing up tsconfig
    
    * Update hu-HU.ts

--- quartz/i18n/index.ts
+++ quartz/i18n/index.ts
@@ -13,6 +13,7 @@ import ru from "./locales/ru-RU"
 import ko from "./locales/ko-KR"
 import zh from "./locales/zh-CN"
 import vi from "./locales/vi-VN"
+import hu from "./locales/hu-HU"
 
 export const TRANSLATIONS = {
   "en-US": en,
@@ -50,6 +51,7 @@ export const TRANSLATIONS = {
   "ko-KR": ko,
   "zh-CN": zh,
   "vi-VN": vi,
+  "hu-HU": hu,
 } as const
 
 export const defaultTranslation = "en-US"
