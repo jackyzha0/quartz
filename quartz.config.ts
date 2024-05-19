bf582cd3bf4818a1340048bce36783ca0b8be89c
mohankumarpaluru <mohankumarpaluru@gmail.com>
2024-05-19 17:22:07 +0530

    updated-naming

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "📝 Matrix Notes",
+    pageTitle: "📚 Curiosity Chronicles",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
