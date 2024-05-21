6227ecaf16382629ad2649b005a70a57e4d908f7
Trần Đức Nam <me@dynamotn.dev>
2024-05-22 00:26:27 +0700

    feat: show banner of notes

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -21,6 +21,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
+    Component.Banner(),
   ],
   left: [
     Component.PageTitle(),
