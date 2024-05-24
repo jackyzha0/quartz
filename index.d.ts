1fd84ccd15391ff97f4212df65d5c9c8eb034125
Sayak Chowdhury <sayakchowdhury384@gmail.com>
2024-05-24 20:20:20 +0000

    updated using prettier

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,12 @@
 declare module "*.scss" {
-  const content: string
-  export = content
+  const content: string;
+  export = content;
 }
 
 // dom custom event
 interface CustomEventMap {
-  nav: CustomEvent<{ url: FullSlug }>
-  themechange: CustomEvent<{ theme: "light" | "dark" }>
+  nav: CustomEvent<{ url: FullSlug }>;
+  themechange: CustomEvent<{ theme: "light" | "dark" }>;
 }
 
-declare const fetchData: Promise<ContentIndex>
+declare const fetchData: Promise<ContentIndex>;
