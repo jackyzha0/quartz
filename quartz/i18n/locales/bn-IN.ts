48cbbb2eb38887f8f2295bd0fb72718b0490750b
Sayak Chowdhury <sayakchowdhury384@gmail.com>
2024-05-24 20:15:11 +0000

    added home broperty to error under translation

--- quartz/i18n/locales/bn-IN.ts
+++ quartz/i18n/locales/bn-IN.ts
@@ -65,6 +65,7 @@ export default {
     error: {
       title: "উৎস অনুপস্থিত",
       notFound: "এই পৃষ্ঠা ব্যক্তিগত বা অস্তিত্বহীন।",
+      home: "হোমপেজে ফিরে যান"
     },
     folderContent: {
       folder: "ফোল্ডার",
