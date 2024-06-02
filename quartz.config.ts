cd175bd016ab366dd8a428084aa10c22b984e0a0
Rami Maalouf <rami.maalouf@ibm.com>
2024-06-02 18:55:23 -0400

    Quartz sync: Jun 2, 2024, 6:55 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,5 +1,5 @@
-import { QuartzConfig } from "./quartz/cfg"
-import * as Plugin from "./quartz/plugins"
+import { QuartzConfig } from "./quartz/cfg";
+import * as Plugin from "./quartz/plugins";
 
 /**
  * Quartz 4.0 Configuration
@@ -7,85 +7,99 @@ import * as Plugin from "./quartz/plugins"
  * See https://quartz.jzhao.xyz/configuration for more information.
  */
 const config: QuartzConfig = {
-  configuration: {
-    pageTitle: "Welcome To Rami's Universe",
-    enableSPA: true,
-    enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian", "Hidden", "My Calendar", "My Social Circle", "USV", "My Goals", "My Projects", "My Files", "My Input Collections", "My Kanbans", "My Views"],
-    defaultDateType: "created",
-    theme: {
-      fontOrigin: "googleFonts",
-      cdnCaching: true,
-      typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
-      },
-      colors: {
-        lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-        },
-        darkMode: {
-          light: "#161618",
-          lightgray: "#393639",
-          gray: "#646464",
-          darkgray: "#d4d4d4",
-          dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-        },
-      },
-    },
-  },
-  plugins: {
-    transformers: [
-      Plugin.FrontMatter(),
-      Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "filesystem"],
-      }),
-      Plugin.SyntaxHighlighting({
-        theme: {
-          light: "github-light",
-          dark: "github-dark",
-        },
-        keepBackground: false,
-      }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
-      Plugin.GitHubFlavoredMarkdown(),
-      Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
-      Plugin.Description(),
-      Plugin.Latex({ renderEngine: "katex" }),
-    ],
-    filters: [Plugin.RemoveDrafts()],
-    emitters: [
-      Plugin.AliasRedirects(),
-      Plugin.ComponentResources(),
-      Plugin.ContentPage(),
-      Plugin.FolderPage(),
-      Plugin.TagPage(),
-      Plugin.ContentIndex({
-        enableSiteMap: true,
-        enableRSS: true,
-      }),
-      Plugin.Assets(),
-      Plugin.Static(),
-      Plugin.NotFoundPage(),
-    ],
-  },
-}
+	configuration: {
+		pageTitle: "Welcome To Rami's Universe",
+		enableSPA: true,
+		enablePopovers: true,
+		analytics: {
+			provider: "plausible",
+		},
+		locale: "en-US",
+		baseUrl: "",
+		ignorePatterns: [
+			"private",
+			"templates",
+			".obsidian",
+			"Hidden",
+			"My Calendar",
+			"My Social Circle",
+			"USV",
+			"My Goals",
+			"My Projects",
+			"My Files",
+			"My Input Collections",
+			"My Kanbans",
+			"My Views",
+		],
+		defaultDateType: "created",
+		theme: {
+			fontOrigin: "googleFonts",
+			cdnCaching: true,
+			typography: {
+				header: "Schibsted Grotesk",
+				body: "Source Sans Pro",
+				code: "IBM Plex Mono",
+			},
+			colors: {
+				lightMode: {
+					light: "#faf8f8",
+					lightgray: "#e5e5e5",
+					gray: "#b8b8b8",
+					darkgray: "#4e4e4e",
+					dark: "#2b2b2b",
+					secondary: "#284b63",
+					tertiary: "#84a59d",
+					highlight: "rgba(143, 159, 169, 0.15)",
+				},
+				darkMode: {
+					light: "#161618",
+					lightgray: "#393639",
+					gray: "#646464",
+					darkgray: "#d4d4d4",
+					dark: "#ebebec",
+					secondary: "#7b97aa",
+					tertiary: "#84a59d",
+					highlight: "rgba(143, 159, 169, 0.15)",
+				},
+			},
+		},
+	},
+	plugins: {
+		transformers: [
+			Plugin.FrontMatter(),
+			Plugin.CreatedModifiedDate({
+				priority: ["frontmatter", "filesystem"],
+			}),
+			Plugin.SyntaxHighlighting({
+				theme: {
+					light: "github-light",
+					dark: "github-dark",
+				},
+				keepBackground: false,
+			}),
+			Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+			Plugin.GitHubFlavoredMarkdown(),
+			Plugin.TableOfContents(),
+			Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+			Plugin.Description(),
+			Plugin.Latex({ renderEngine: "katex" }),
+		],
+		filters: [Plugin.RemoveDrafts()],
+		emitters: [
+			Plugin.AliasRedirects(),
+			Plugin.ComponentResources(),
+			Plugin.ContentPage(),
+			Plugin.FolderPage(),
+			Plugin.TagPage(),
+			Plugin.ContentIndex({
+				enableSiteMap: true,
+				enableRSS: true,
+			}),
+			Plugin.Assets(),
+			Plugin.Static(),
+			Plugin.NotFoundPage(),
+		],
+	},
+};
 
-export default config
+export default config;
