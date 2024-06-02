2f6267e863838f5b0dcda9c2750c07dab2b20d44
Rami Maalouf <rami.maalouf@ibm.com>
2024-06-02 18:37:36 -0400

    first test push

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Welcome To Rami's Universe",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "Hidden", "My Calendar", "My Social Circle", "USV", "My Goals", "My Projects", "My Files", "My Input Collections", "My Kanbans", "My Views"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
