5a08162925cac86c0f473832f54e3717a38f0349
Carson Clarke-Magrab <carsonclarke570@gmail.com>
2024-06-12 13:52:04 -0400

    vercel stuff

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "blog.cclarke-magrab.me",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
