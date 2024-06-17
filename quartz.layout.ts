d2631cb6cdf1cc622f98ac19482bdaa7a89b0347
Miguel Pimentel <contact@miguelpimentel.do>
2024-06-17 15:25:39 -0500

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,5 +1,5 @@
-import {PageLayout, SharedLayout} from "./quartz/cfg"
-import * as Component from "./quartz/components"
+import { PageLayout, SharedLayout } from "./quartz/cfg";
+import * as Component from "./quartz/components";
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
@@ -12,20 +12,20 @@ export const sharedPageComponents: SharedLayout = {
       Blog: "https://miguelpimentel.do/",
       Meta: "/Meta",
       GitHub: "https://github.com/semanticdata/forgetful-notes/",
-      // Journal: "/journal",
-      // Source: "https://github.com/semanticdata/forgetful-notes/",
     },
   }),
-}
+};
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [Component.ArticleTitle()],
-  left: [Component.PageTitle(),
-         Component.Search(),
-         Component.Darkmode(),
-        Component.MobileOnly(Component.Spacer()),
-        Component.DesktopOnly(Component.Explorer())],
+  left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Search(),
+    Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer()),
+  ],
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
@@ -50,17 +50,19 @@ export const defaultContentPageLayout: PageLayout = {
   //   Component.DesktopOnly(Component.TableOfContents()),
   //   Component.Backlinks(),
   // ],
-}
+};
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.ArticleTitle()],
-  left: [Component.PageTitle(),
-         Component.Search(),
-         Component.Darkmode(),
-         Component.MobileOnly(Component.Spacer()),
-         Component.DesktopOnly(Component.Explorer())],
-  right: [Component.MobileOnly(Component.Spacer())],
+  left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Search(),
+    Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer()),
+  ],
+  right: [],
 
   // beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   // left: [
@@ -71,4 +73,4 @@ export const defaultListPageLayout: PageLayout = {
   //   Component.DesktopOnly(Component.Explorer()),
   // ],
   // right: [],
-}
+};
