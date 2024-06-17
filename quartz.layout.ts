5525fe23eb53d8789f0ba6e8598fbfa81e25b21a
Miguel Pimentel <contact@miguelpimentel.do>
2024-06-17 15:13:52 -0500

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,7 +4,8 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [Component.PageTitle(), Component.Search(), Component.Darkmode()],
+  // header: [Component.PageTitle(), Component.Search(), Component.Darkmode()],
+  header: [],
   footer: Component.Footer({
     links: {
       About: "/About",
@@ -20,13 +21,15 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [Component.ArticleTitle()],
-  left: [Component.MobileOnly(Component.Spacer()),
+  left: [Component.PageTitle(),
+         Component.Search(),
+         Component.Darkmode()
+        Component.MobileOnly(Component.Spacer()),
         Component.DesktopOnly(Component.Explorer())],
   right: [
-  //  Component.MobileOnly(Component.Graph()),
-  // Component.MobileOnly(Component.Backlinks()),
     Component.Graph(),
-    Component.Backlinks()
+    Component.DesktopOnly(Component.TableOfContents()),
+    Component.Backlinks(),
   ],
 
   // beforeBody: [
@@ -52,8 +55,11 @@ export const defaultContentPageLayout: PageLayout = {
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.ArticleTitle()],
-  left: [Component.MobileOnly(Component.Spacer()),
-        Component.DesktopOnly(Component.Explorer())],
+  left: [Component.PageTitle(),
+         Component.Search(),
+         Component.Darkmode(),
+         Component.MobileOnly(Component.Spacer()),
+         Component.DesktopOnly(Component.Explorer())],
   right: [Component.MobileOnly(Component.Spacer())],
 
   // beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
