31249e4c30487ff03f648d7c9b58fa6ede6e32a0
Peter Stather <github@stather.me>
2024-06-29 01:39:19 +0100

    Add textHighlight theme setting

--- quartz.config.ts
+++ quartz.config.ts
@@ -36,6 +36,7 @@ const config: QuartzConfig = {
           secondary: "#284b63",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
+          textHighlight: "#fff23688",
         },
         darkMode: {
           light: "#161618",
@@ -46,6 +47,7 @@ const config: QuartzConfig = {
           secondary: "#7b97aa",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
+          textHighlight: "#b3aa0288",
         },
       },
     },
