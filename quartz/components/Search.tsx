ac503f7b1307fa069722fc1d8763a57836e26825
ndrooo <git@ndr.ooo>
2024-08-09 19:30:52 -0400

    Run prettier on Search.tsx

--- quartz/components/Search.tsx
+++ quartz/components/Search.tsx
@@ -21,11 +21,7 @@ export default ((userOpts?: Partial<SearchOptions>) => {
       <div class={classNames(displayClass, "search")}>
         <button class="search-button" id="search-button">
           <p>{i18n(cfg.locale).components.search.title}</p>
-          <svg
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 19.9 19.7"
-          >
+          <svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.9 19.7">
             <title>Search</title>
             <g class="search-path" fill="none">
               <path stroke-linecap="square" d="M18.5 18.3l-5.4-5.4" />
