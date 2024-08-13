e83dcc2751c637f89dcc95caafb901d33a5ee8fc
ndrooo <git@ndr.ooo>
2024-08-12 21:39:11 -0400

    Add a wrapper span to dates in PageList.
    This means there is a placeholder when date is not specified, so the values in grid-template-columns always line up correctly.

--- quartz/components/PageList.tsx
+++ quartz/components/PageList.tsx
@@ -46,11 +46,13 @@ export const PageList: QuartzComponent = ({ cfg, fileData, allFiles, limit, sort
         return (
           <li class="section-li">
             <div class="section">
-              {page.dates && (
-                <p class="meta">
-                  <Date date={getDate(cfg, page)!} locale={cfg.locale} />
-                </p>
-              )}
+              <span>
+                {page.dates && (
+                  <p class="meta">
+                    <Date date={getDate(cfg, page)!} locale={cfg.locale} />
+                  </p>
+                )}
+              </span>
               <div class="desc">
                 <h3>
                   <a href={resolveRelative(fileData.slug!, page.slug!)} class="internal">
