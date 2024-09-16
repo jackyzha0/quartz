002e9b50fdc4c677d4cdf9dc46b9fb0bd562f5dc
saberzero1 <github@emilebangma.com>
2024-09-16 10:50:11 +0200

    Addressed PR feedback

--- quartz/components/Backlinks.tsx
+++ quartz/components/Backlinks.tsx
@@ -14,22 +14,20 @@ const Backlinks: QuartzComponent = ({
   const backlinkFiles = allFiles.filter((file) => file.links?.includes(slug))
   return (
     <div class={classNames(displayClass, "backlinks")}>
-      <div class="backlinks-container">
-        <h3>{i18n(cfg.locale).components.backlinks.title}</h3>
-        <ul class="overflow">
-          {backlinkFiles.length > 0 ? (
-            backlinkFiles.map((f) => (
-              <li>
-                <a href={resolveRelative(fileData.slug!, f.slug!)} class="internal">
-                  {f.frontmatter?.title}
-                </a>
-              </li>
-            ))
-          ) : (
-            <li>{i18n(cfg.locale).components.backlinks.noBacklinksFound}</li>
-          )}
-        </ul>
-      </div>
+      <h3>{i18n(cfg.locale).components.backlinks.title}</h3>
+      <ul class="overflow">
+        {backlinkFiles.length > 0 ? (
+          backlinkFiles.map((f) => (
+            <li>
+              <a href={resolveRelative(fileData.slug!, f.slug!)} class="internal">
+                {f.frontmatter?.title}
+              </a>
+            </li>
+          ))
+        ) : (
+          <li>{i18n(cfg.locale).components.backlinks.noBacklinksFound}</li>
+        )}
+      </ul>
     </div>
   )
 }
