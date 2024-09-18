adfe69e13711e3c3ad37cd66b287f67c864f6f3c
Emile Bangma <ewjbangma@hotmail.com>
2024-09-18 18:16:59 +0000

    Initial setup for markdown parser rewrite

---
+++ quartz/plugins/transformers/markdown.ts
@@ -0,0 +1,46 @@
+import { QuartzTransformerPlugin } from "../types"
+
+export interface Options {
+  option1: Boolean
+}
+
+const defaultOptions: Options = {
+  option1: true,
+}
+
+export const GitHubFlavoredMarkdown: QuartzTransformerPlugin<Partial<Options>> = (userOpts) => {
+  const opts = { ...defaultOptions, ...userOpts }
+  return {
+    name: "GitHubFlavoredMarkdown",
+  }
+}
+
+export const ObsidianFlavoredMarkdown: QuartzTransformerPlugin<Partial<Options>> = (userOpts) => {
+  const opts = { ...defaultOptions, ...userOpts }
+  return {
+    name: "ObsidianFlavoredMarkdown",
+  }
+}
+
+export const RoamFlavoredMarkdown: QuartzTransformerPlugin<Partial<Options> | undefined> = (
+  userOpts,
+) => {
+  const opts = { ...defaultOptions, ...userOpts }
+  return {
+    name: "RoamFlavoredMarkdown",
+  }
+}
+
+export const OxHugoFlavoredMarkdown: QuartzTransformerPlugin<Partial<Options>> = (userOpts) => {
+  const opts = { ...defaultOptions, ...userOpts }
+  return {
+    name: "OxHugoFlavoredMarkdown",
+  }
+}
+
+export const CustomFlavoredMarkdown: QuartzTransformerPlugin<Partial<Options>> = (userOpts) => {
+  const opts = { ...defaultOptions, ...userOpts }
+  return {
+    name: "CustomFlavoredMarkdown",
+  }
+}
