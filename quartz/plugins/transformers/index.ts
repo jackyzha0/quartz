03f4cb8b8ad95366316e99df97449b7f68312ffe
Emile Bangma <ewjbangma@hotmail.com>
2024-09-19 09:46:05 +0000

    Parser index

--- quartz/plugins/transformers/index.ts
+++ quartz/plugins/transformers/index.ts
@@ -1,15 +1,17 @@
 export { FrontMatter } from "./frontmatter"
-export { GitHubFlavoredMarkdown } from "./markdown"
+export {
+  CommonMarkFlavoredMarkdown,
+  CustomFlavoredMarkdown,
+  GitHubFlavoredMarkdown,
+  ObsidianFlavoredMarkdown,
+  OxHugoFlavoredMarkdown,
+  RoamFlavoredMarkdown,
+} from "./markdown"
 export { Citations } from "./citations"
 export { CreatedModifiedDate } from "./lastmod"
 export { Latex } from "./latex"
 export { Description } from "./description"
 export { CrawlLinks } from "./links"
-export { ObsidianFlavoredMarkdown } from "./markdown"
-export { OxHugoFlavoredMarkdown } from "./markdown"
 export { SyntaxHighlighting } from "./syntax"
 export { TableOfContents } from "./toc"
 export { HardLineBreaks } from "./linebreaks"
-export { RoamFlavoredMarkdown } from "./markdown"
-export { CommonMarkFlavoredMarkdown } from "./markdown"
-export { CustomFlavoredMarkdown } from "./markdown"
