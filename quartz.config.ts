921f45cf7001a6af77cd0bf10f7fbf154eebabff
threehymns <70611435+threehymns@users.noreply.github.com>
2024-09-24 20:20:36 -0400

    feat: add a config option for a pageTitleSuffix (#1320)
    * feat: add a config option for a pageTitleSuffix
    
    * Run Prettier on Head.tsx
    
    * Make pageTitleSuffix optional
    
    Co-authored-by: Aaron Pham <Aaronpham0103@gmail.com>
    
    ---------
    
    Co-authored-by: Aaron Pham <Aaronpham0103@gmail.com>

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,6 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "🪴 Quartz 4.0",
+    pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
