67e1beea7052ea515d47b881d4a30fa3ed847b0b
Emile Bangma <github@emilebangma.com>
2024-10-18 00:30:16 +0200

    feat(comments): support custom giscus themes (#1526)
    Co-authored-by: Aaron Pham <Aaronpham0103@gmail.com>
    Co-authored-by: Aaron Pham <contact@aarnphm.xyz>

--- quartz/components/Comments.tsx
+++ quartz/components/Comments.tsx
@@ -10,6 +10,9 @@ type Options = {
     repoId: string
     category: string
     categoryId: string
+    themeUrl?: string
+    lightTheme?: string
+    darkTheme?: string
     mapping?: "url" | "title" | "og:title" | "specific" | "number" | "pathname"
     strict?: boolean
     reactionsEnabled?: boolean
@@ -34,6 +37,11 @@ export default ((opts: Options) => {
         data-strict={boolToStringBool(opts.options.strict ?? true)}
         data-reactions-enabled={boolToStringBool(opts.options.reactionsEnabled ?? true)}
         data-input-position={opts.options.inputPosition ?? "bottom"}
+        data-light-theme={opts.options.lightTheme ?? "light"}
+        data-dark-theme={opts.options.darkTheme ?? "dark"}
+        data-theme-url={
+          opts.options.themeUrl ?? `https://${cfg.baseUrl ?? "example.com"}/static/giscus`
+        }
       ></div>
     )
   }
