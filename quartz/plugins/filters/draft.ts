ad52d095670f2a08a330d1d4ee5ed4bc784e65aa
Emile Bangma <ewjbangma@hotmail.com>
2024-10-22 20:33:54 +0000

    Feat(filters): Support "true" as valid for draft and publish frontmatter values (b3a02909ba74fff08cd3675707d1f4d782a24e98)

--- quartz/plugins/filters/draft.ts
+++ quartz/plugins/filters/draft.ts
@@ -3,7 +3,8 @@ import { QuartzFilterPlugin } from "../types"
 export const RemoveDrafts: QuartzFilterPlugin<{}> = () => ({
   name: "RemoveDrafts",
   shouldPublish(_ctx, [_tree, vfile]) {
-    const draftFlag: boolean = vfile.data?.frontmatter?.draft === true
+    const draftFlag: boolean =
+      vfile.data?.frontmatter?.draft === true || vfile.data?.frontmatter?.draft === "true"
     return !draftFlag
   },
 })
